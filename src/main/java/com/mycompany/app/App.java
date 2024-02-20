d451de95f6dfa85c880f25cb54ce351f99692369
Shawn Lim <shawnlfs90@gmail.com>
2024-02-20 15:25:03 +0800

    Commit change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java app. Success.");
+        System.out.println("App is updated.");
     }
 
     public String getMessage() {
