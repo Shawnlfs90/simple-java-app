969296844301002774522ceccb7a2792c508e2e6
Shawn Lim <shawnlfs90@gmail.com>
2024-02-20 16:33:53 +0800

    Commit polling  test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("App is updated.");
+        System.out.println("App is updated for polling.");
     }
 
     public String getMessage() {
